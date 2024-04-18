00c758cb47e81326297b504981a21057989c410a
larryacademy001 <larryacademy@gmail.com>
2024-04-18 02:12:11 +0100

    Update console.py

--- console.py
+++ console.py
@@ -94,7 +94,7 @@ class HBNBCommand(cmd.Cmd):
         return stop
 
     def do_quit(self, command):
-        """Method to exit the HBNB console."""
+        """Function to exit the HBNB console."""
         exit()
 
     def help_quit(self):
@@ -102,12 +102,12 @@ class HBNBCommand(cmd.Cmd):
         print("Exits the program with formatting\n")
 
     def do_EOF(self, arg):
-        """Handles EOF to exit program."""
+        """Handle EOF to exit program."""
         print()
         exit()
 
     def help_EOF(self):
-        """ Prints the help documentation for EOF """
+        """Print the help documentation for EOF."""
         print("Exits the program without formatting\n")
 
     def emptyline(self):
