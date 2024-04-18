fc0d8fbb9389a3e5ae17cb7bfb6bf95df279ff15
larryacademy001 <larryacademy@gmail.com>
2024-04-18 02:42:22 +0100

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" DB Storage for sqlalchemy """
+"""DB Storage for sqlalchemy."""
 from os import getenv
 from sqlalchemy import (create_engine)
 from sqlalchemy.ext.declarative import declarative_base
