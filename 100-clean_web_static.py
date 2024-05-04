1e6a56c83f2db99ca21fb2ab0871f09d1e919920
Larry Academy <larryacademy@gmail.com>
2024-05-04 03:09:42 +0100

    Update

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -8,7 +8,7 @@ fab -f 100-clean_web_static.py do_clean:number=2
 import os
 from fabric.api import *
 
-env.hosts = ['52.87.155.66', '54.89.109.87']
+env.hosts = ['52.204.70.156', '34.227.91.238']
 
 
 def do_clean(number=0):
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ archive to the web servers
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['54.89.109.87', '100.25.190.21']
+env.hosts = ['52.204.70.156', '34.227.91.238']
 
 
 def do_deploy(archive_path):
