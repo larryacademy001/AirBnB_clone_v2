8a6eece65f93630986677065170ac5df7bd8ca8d
Larry Academy <larryacademy@gmail.com>
2024-05-04 01:54:00 +0100

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 """
-Fabric script that generates a .tgz archive from the
-contents of the web_static folder of your AirBnB Clone
-repo, using the function do_pack.
+Fabric script that generates a .tgz
+archive from the contents of the web_static
+folder of your AirBnB Clone repo, using the
+function do_pack
 """
 
-from datec_time import datec_time
+from datetime import datetime
 from fabric.api import *
 
 
 def do_pack():
-    """
-    Creating archives in web_static
-    """
+    """making an archive on web_static folder"""
 
-    c_time = datec_time.now()
-    archive = 'web_static_' + c_time.strfc_time("%Y%m%d%H%M%S") + '.' + 'tgz'
+    time = datetime.now()
+    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
     local('mkdir -p versions')
     create = local('tar -cvzf versions/{} web_static'.format(archive))
     if create is not None:
