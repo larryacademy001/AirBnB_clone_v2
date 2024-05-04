9b1c61e8609b15e7ac0314e0b9f8144277b71f1e
Larry Academy <larryacademy@gmail.com>
2024-05-04 01:51:25 +0100

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,7 +5,7 @@ contents of the web_static folder of your AirBnB Clone
 repo, using the function do_pack.
 """
 
-from datecur_time import datecur_time
+from datec_time import datec_time
 from fabric.api import *
 
 
@@ -14,8 +14,8 @@ def do_pack():
     Creating archives in web_static
     """
 
-    cur_time = datecur_time.now()
-    archive = 'web_static_' + cur_time.strfcur_time("%Y%m%d%H%M%S") + '.' + 'tgz'
+    c_time = datec_time.now()
+    archive = 'web_static_' + c_time.strfc_time("%Y%m%d%H%M%S") + '.' + 'tgz'
     local('mkdir -p versions')
     create = local('tar -cvzf versions/{} web_static'.format(archive))
     if create is not None:
